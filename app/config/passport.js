84ac457d19fa0e6f173e293adc78004bbe3ac24b
Björn von der Linde <bjoern-hendrik.von.der.linde@exhumedo.de>
2016-04-08 15:00:16 +0200

    added twitter login

--- app/config/passport.js
+++ app/config/passport.js
@@ -1,5 +1,6 @@
 var LocalStrategy 	 = require('passport-local').Strategy;
 var FacebookStrategy = require('passport-facebook').Strategy;
+var TwitterStrategy  = require('passport-twitter').Strategy;
 var User 			 = require('../models/user');
 var auth 			 = require('./auth');
 
@@ -109,4 +110,47 @@ module.exports = function(passport) {
         });
 
     }));
+
+    passport.use(new TwitterStrategy({
+        consumerKey     : auth.twitterAuth.consumerKey,
+        consumerSecret  : auth.twitterAuth.consumerSecret,
+        callbackURL     : auth.twitterAuth.callbackURL
+
+    }, function(token, tokenSecret, profile, done) {
+        
+        process.nextTick(function() {
+
+            User.findOne({ 'twitter.id' : profile.id }, function(err, user) {
+
+                // if there is an error, stop everything and return that
+                // ie an error connecting to the database
+                if (err)
+                    return done(err);
+
+                // if the user is found then log them in
+                if (user) {
+                    return done(null, user); // user found, return that user
+                } else {
+                    // if there is no user, create them
+                    var newUser                 = new User();
+
+                    // set all of the user data that we need
+                    newUser.twitter.id          = profile.id;
+                    newUser.twitter.token       = token;
+                    newUser.twitter.username    = profile.username;
+                    newUser.twitter.displayName = profile.displayName;
+
+                    // save our user into the database
+                    newUser.save(function(err) {
+                        if (err)
+                            throw err;
+                        return done(null, newUser);
+                    });
+                }
+            });
+
+    });
+
+    }));
+
 }
--- app/routes.js
+++ app/routes.js
@@ -60,13 +60,22 @@ module.exports = function(app, passport) {
         passport.authenticate('facebook', {
             successRedirect : '/profile',
             failureRedirect : '/'
-        }));
+        })
+    );
 
-    // route for logging out
-    app.get('/logout', function(req, res) {
-        req.logout();
-        res.redirect('/');
-    });
+    // =====================================
+    // TWITTER ROUTES ======================
+    // =====================================
+    // route for twitter authentication and login
+    app.get('/auth/twitter', passport.authenticate('twitter'));
+
+    // handle the callback after twitter has authenticated the user
+    app.get('/auth/twitter/callback',
+        passport.authenticate('twitter', {
+            successRedirect : '/profile',
+            failureRedirect : '/'
+        })
+    );
 
     // =====================================
     // LOGOUT ==============================
